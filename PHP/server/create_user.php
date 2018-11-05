40ed806a9d4be29aaadeb811f535a3f9fe3231fb
Wilmar yepes <wilyps1@yahoo.com>
2018-11-05 12:00:57 -0500

    Initial commit

---
+++ PHP/server/create_user.php
@@ -0,0 +1,24 @@
+<?php
+
+    include 'conexionBD.php';
+    CrearUsuarios("carlos","1993/07/16","cjcs@mis.com","123456");
+    CrearUsuarios("Pepito Perez","1992/01/16","Pepito@msn.com","123456");
+    CrearUsuarios("Roberto Gomez","1990/01/16","Robert@msn.com","123456");
+
+    function CrearUsuarios($Nombre,$FechaNacimiento,$UserName,$Password){
+    IniciarConexion();
+    $Consulta="Select * from usuario where Username='".$UserName."'";
+    $Resultado= mysqli_num_rows($GLOBALS['Conexion']->query($Consulta));
+    if($Resultado==0){
+        $Consulta = "INSERT INTO usuario (Nombre, FechaNacimiento, Username, Password)
+        VALUES ('".$Nombre."', '".$FechaNacimiento."', '".$UserName."', '".password_hash($Password, PASSWORD_BCRYPT)."')";
+
+        if ($GLOBALS['Conexion']->query($Consulta) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $GLOBALS['Conexion']->error;
+        }
+    }
+    DesactivarConexion();
+    }
+ ?>
